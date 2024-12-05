a5f0733cd6502c9c8787e7851a15d704cd348d80
Katherine fang <katherinefang@Kats-MacDonald.local>
2024-12-04 19:25:48 -0800

    a6

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ import UserRoutes from "./Kanbas/Users/routes.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentsRoutes from './Kanbas/Assignments/routes.js';
+import EnrollmentsRoutes from './Kanbas/Enrollments/routes.js';
 import session from "express-session";
 import "dotenv/config";
-import EnrollmentsRoutes from './Kanbas/Enrollments/routes.js';
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 
 app.use(cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    origin: "http://localhost:3000",
+    //    origin: process.env.NETLIFY_URL || "http://localhost:3000",
     //origin: 'https://singular-marzipan-9747a2.netlify.app/'
 })); 
 const sessionOptions = {
